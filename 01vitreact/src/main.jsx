7866abc761c0baa179aa27a12c9b606f7fb9c11a
tarjan-1 <tarjanbawankule1@gmail.com>
2023-10-26 18:01:07 +0530

    learned about internal react render method

--- 01vitreact/src/main.jsx
+++ 01vitreact/src/main.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function myApp(){
+//   return <div>
+//     <h1>Custom App | chai </h1>
+//   </div>
+// }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* <myApp /> */}
   </React.StrictMode>,
 )
